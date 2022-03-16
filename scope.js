1f152c7b563e782fa765328923a9b2194d166287
Serge Ntumba <s7ntumba@gmail.com>
2022-03-16 10:22:59 +0100

    initial commit

---
+++ scope.js
@@ -0,0 +1,11 @@
+const myName = "John";
+
+function first() {
+  const age = 27;
+  console.log(myName);
+  if (age > 18) {
+    console.log(`${myName} peut boire parce qu'il a ${age} ans`);
+  }
+}
+
+first();
